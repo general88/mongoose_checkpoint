2078e275948c675633330b298e2c989596bf1de5
ogunTee <adeniyioguntamu@gmail.com>
2024-01-03 14:20:21 +0100

    db edit

--- dbConnect.js
+++ dbConnect.js
@@ -2,17 +2,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-});
-
-// Check if the connection is successful
-const connectDB = mongoose.connection;
-
-connectDB.on("error", console.error.bind(console, "MongoDB connection error:"));
-connectDB.once("open", function () {
-  console.log("Connected to the database");
-});
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("connection to database successful");
+};
 
 module.exports = { connectDB };
